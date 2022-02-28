874e575476abbe53fbdda2506fe7d7b10bf09b36
Johann <johanndumoulin734@gmail.com>
2022-02-28 21:40:15 +0100

    Fix user account

--- src/Controller/UserAccountController.php
+++ src/Controller/UserAccountController.php
@@ -4,16 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class UserAccountController extends AbstractController
 {
-    /**
-     * @Route({
-        "en": "/account",
-     *  "fr": "/mon-compte"
-     * }, name="user_account")
-     */
     public function index(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
